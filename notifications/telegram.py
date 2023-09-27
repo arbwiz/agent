e361d25309a7b2435b489ca2e2290eea601eac8b
João Ferreira Oliveira <joao.ferreira.oliveira@fanduel.com>
2023-09-27 12:14:37 +0100

    format cenas
    format

--- notifications/telegram.py
+++ notifications/telegram.py
@@ -1,21 +1,23 @@
 import requests
 import json
 import urllib3
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-#6480489284
+
+# 6480489284
 def send_telegram_message(message):
     send_telegram_message_complex(message,
-                          "4010862393",
-                          "6613908503:AAGLsuazZhBmwHbPCEZzx-tJGQHLOtEcZy0")
+                                  "4010862393",
+                                  "6613908503:AAGLsuazZhBmwHbPCEZzx-tJGQHLOtEcZy0")
+
 
 def send_telegram_message_complex(message: str,
-                          chat_id: str,
-                          api_key: str,
-                          proxy_username: str = None,
-                          proxy_password: str = None,
-		  proxy_url: str = None):
-
+                                  chat_id: str,
+                                  api_key: str,
+                                  proxy_username: str = None,
+                                  proxy_password: str = None,
+                                  proxy_url: str = None):
     proxies = None
     if proxy_url is not None:
         proxies = {
@@ -23,17 +25,17 @@ def send_telegram_message_complex(message: str,
             'http': f'http://{proxy_username}:{proxy_password}@{proxy_url}'
         }
     headers = {'Content-Type': 'application/json',
-                'Proxy-Authorization': 'Basic base64'}
+               'Proxy-Authorization': 'Basic base64'}
     data_dict = {'chat_id': chat_id,
-                    'text': message,
-                    'parse_mode': 'HTML',
-                    'disable_notification': True}
-                    
+                 'text': message,
+                 'parse_mode': 'HTML',
+                 'disable_notification': True}
+
     data = json.dumps(data_dict)
     url = f'https://api.telegram.org/bot{api_key}/sendMessage'
     response = requests.post(url,
-                                data=data,
-                                headers=headers,
-                                proxies=proxies,
-                                verify=False)
+                             data=data,
+                             headers=headers,
+                             proxies=proxies,
+                             verify=False)
     return response
